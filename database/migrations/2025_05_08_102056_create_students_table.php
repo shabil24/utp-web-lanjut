139a4ed06a1636666981feecf373aa27e37178a1
unknown <200203526+shabil24@users.noreply.github.com>
2025-05-08 17:45:40 +0700

    Part 2

---
+++ database/migrations/2025_05_08_102056_create_students_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->id();
+            $table->string('nama');
+            $table->string('address');
+            $table->string('mobile');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
